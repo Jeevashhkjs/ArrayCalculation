e930163221cb64b474b36368bf581bf73b92603a
jeevashhkjs <jeevasdckap@gmail.com>
2023-05-22 16:50:34 +0530

    recursion function working well

--- arrayCalculation.php
+++ arrayCalculation.php
@@ -8,49 +8,55 @@ $array = [
     [56, -1, 66, -1, 67], // => 66
 ];
 
-for($i=0;$i<count($array);$i++){
-    for($j=0;$j<count($array);$j++){
-        if($array[$i][$j] == -1){
-            print_r($j);
+$outputArray = [];
+
+$count = 0;
+
+function findBiggestNumber($array){
+    $GLOBALS['count']++;
+    if($GLOBALS['count'] < count($array)){
+        findBiggestNumber($GLOBALS['array'][$GLOBALS['count']]);
+    }
+
+    $indexArray = [];
+    $values = [];
+    for($i=0;$i<count($array);$i++){
+        if($array[$i] == -1) {
+            array_push($indexArray, $i);
         }
+    }
+
+    $lastIndex = count($indexArray);
+
+    if($lastIndex == 1){
+        $lastIndex = count($array);
+
+    }else{
+        $lastIndex = $indexArray[1];
+    }
+
+    for($k=0;$k<count($array);$k++){
+        if($k>$indexArray[0] && $k<$lastIndex){
+            array_push($values,$array[$k]);
+        }
+    }
+    $output = array_sum($values);
+
+    $GLOBALS['outputArray'][] = $output;
+
+
+
+
+}
+findBiggestNumber($array[$count]);
+
+$BiggestNumber = 0;
+
+for($js=0;$js<count($outputArray);$js++){
+    if($outputArray[$js] > $BiggestNumber){
+        $BiggestNumber = $outputArray[$js];
     }
 }
 
-//$count = 0;
-//
-//function findBiggestNumber($array){
-//    $GLOBALS['count']++;
-//    if($GLOBALS['count'] < count($array)){
-//        findBiggestNumber($array);
-//    }
-//
-//    $indexArray = [];
-//    $values = [];
-//    for($i=0;$i<count($array);$i++){
-//        if($array[$i] == -1) {
-//            array_push($indexArray, $i);
-//        }
-//    }
-//
-//    $lastIndex = count($indexArray);
-//
-//    if($lastIndex == 1){
-//        $lastIndex = count($array);
-//
-//    }else{
-//        $lastIndex = $indexArray[1];
-//    }
-//
-//    for($k=0;$k<count($array);$k++){
-//        if($k>$indexArray[0] && $k<$lastIndex){
-//            array_push($values,$array[$k]);
-//        }
-//    }
-//    $output = array_sum($values);
-//    echo $output;
-//}
-//
-//findBiggestNumber($GLOBALS['array']);
-
-//print_r($indexArray);
+echo $BiggestNumber;
 
